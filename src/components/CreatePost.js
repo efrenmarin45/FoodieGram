da9cc66e7957cc2573e420a6796cd91153945496
Efren_Marin <efren45marin@gmail.com>
2021-02-22 13:45:55 -0800

    Set up initial components

---
+++ src/components/CreatePost.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CreatePost(){
+    return(
+        <div>Create New Posts</div>
+    )
+}
+
+export default CreatePost;
---
+++ src/components/PostList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PostList(){
+    return(
+        <div>Post List</div>
+    )
+}
+
+export default PostList;
